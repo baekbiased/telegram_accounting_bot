9e7cc79903882f736a9b669ff94edb68357e0e83
Jahanzaib <jahanzaibkhan239@gmail.com>
2021-11-14 23:05:09 +0500

    update

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+$token = '2119894887:AAG8Eh8GhUf7CkKogN_tWn4VH6GsJ9XIvSM';
+$chat_id = '2117661388';
+$message = 'Hello';
+$url = "https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=$message";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+$result = curl_exec($ch);
+curl_close($ch);
+
+$result = json_encode($result, true);
+
+if (isset($result['ok'])){
+    if (isset($result['result'])){
+        echo "D0ne";
+    }
+    else{
+        echo $result['description'];
+    }
+}
+else{
+    echo "Something Went Wrong";
+}
+
+
+
+?>
