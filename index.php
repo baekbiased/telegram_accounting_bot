7dd4f6af6c7752b98cd5383e4009fc586c6c99a4
Jahanzaib <jahanzaibkhan239@gmail.com>
2021-11-14 23:40:33 +0500

    webhook update

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
 
+$input = file_get_contents('php://input');
+$data = json_decode($input);
+
+$chat_id = $data->message->chat->id;
+$message = $data->message->text;
+$message = $message . "Webhook Reply";
+
 $token = '2119894887:AAG8Eh8GhUf7CkKogN_tWn4VH6GsJ9XIvSM';
-$chat_id = '2117661388';
-$message = 'from hereku!!!';
 $url = "https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=$message";
 
 $ch = curl_init();
@@ -10,21 +15,5 @@ curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
 $result = curl_exec($ch);
 curl_close($ch);
-
-$result = json_decode($result, true);
-
-if (isset($result['ok'])){
-    if (isset($result['result'])){
-        echo "D0ne";
-    }
-    else{
-        echo $result['description'];
-    }
-}
-else{
-    echo "Something Went Wrong";
-}
-
-
 
 ?>
