b8fcdc96a07ae3d63904854db5f4521fd0d61958
Jahanzaib <jahanzaibkhan239@gmail.com>
2021-11-15 05:02:11 +0500

    bot commands updated

--- index.php
+++ index.php
@@ -14,12 +14,20 @@ if (str_contains($message, '/prepaid')) {
 }
 elseif (str_contains($message, '/giftCard')) {
     $message = urlencode('Enter in this format: <\br> brand e.g "Visa Card" (required) <\br> Number e.g "123456789" (required) <\br> pin1 e.g "12434" (optional) <\br> pin2 e.g "12434" (optional) <\br> value e.g "2000" (required) <\br> purchase rate e.g "0.7" (required)');
+    $message2 = "e.g. 'giftcard 1234567812345678 1234 5678 2000 0.5 yes 200 save this info'";
 }
 else {
     $message = "Please enter valid command!!!";
 }
 
 $url = "https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=$message";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+$result = curl_exec($ch);
+curl_close($ch);
+
+$url = "https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=$message2";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
